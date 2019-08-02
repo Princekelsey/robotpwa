1b8214f33366f61e6c5de961e1c01155c5c49664
Prince Ajuobi <princekelsey299@gmail.com>
2019-08-03 01:14:03 +0300

    Updates

---
+++ precache-manifest.42414fca1d1b9fa3c7e31aa8ff28854c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24141750a74e9cad8b9702b6298474ca",
+    "url": "/robot-pwa/index.html"
+  },
+  {
+    "revision": "d957ad2f2306b2416b43",
+    "url": "/robot-pwa/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "534e975e724dd6c7213d",
+    "url": "/robot-pwa/static/css/main.a0fbb60e.chunk.css"
+  },
+  {
+    "revision": "d957ad2f2306b2416b43",
+    "url": "/robot-pwa/static/js/2.9f060845.chunk.js"
+  },
+  {
+    "revision": "534e975e724dd6c7213d",
+    "url": "/robot-pwa/static/js/main.2fd2722b.chunk.js"
+  },
+  {
+    "revision": "10654e7a3d3f7923a597",
+    "url": "/robot-pwa/static/js/runtime~main.f32554d2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
